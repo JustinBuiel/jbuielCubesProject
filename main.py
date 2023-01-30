2fb968161d7caafbe3aa1504b8f9dbc29acf690e
unknown <JustinBuiel>
2023-01-30 14:58:22 -0500

    inital setup, test functionality

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import secret
+import requests
+import sys
+from secret import wufoo_key
+from requests.auth import HTTPBasicAuth
 
 
 def main():
-    print(secret.get_api_key())
+    url = "https://justinb.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json"
+    response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, 'pass'))
+
+    if response.status_code != 200:
+        print(
+            f'Failed to get data, response from website: {response.status_code} with error: {response.reason}')
+        sys.exit(-1)
+
+    json_object = response.json()
+    print(json_object)
 
 
 if __name__ == "__main__":
