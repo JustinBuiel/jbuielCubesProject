410884e532c6620fa657f2f0be8b67220f8f4ff1
Justin Buiel <83608864+JustinBuiel@users.noreply.github.com>
2023-01-30 20:32:30 -0500

    Update gather_data.py

--- gather_data.py
+++ gather_data.py
@@ -5,7 +5,7 @@ from requests.auth import HTTPBasicAuth
 
 
 def get_json_data() -> dict:
-    wufoo_key = os.environ['WUFOO_KEY']
+    wufoo_key = os.environ['API_KEY']
     url = "https://justinb.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json"
     response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, 'pass'))
 
